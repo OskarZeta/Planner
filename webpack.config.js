a0bd9a4f720fbab606d4925163a660a5416144e6
Juan <dw4h@yandex.ru>
2019-07-15 15:09:08 +0300

    Added webpack loaders for css handling

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,36 @@
 const path = require('path');
-//const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const mode = process.env.NODE_ENV.trim();
 
 module.exports = {
-  // plugins: [
-  //   new MiniCssExtractPlugin({
-  //     filename: 'bundle.css'
-  //   })
-  // ],
-  entry: path.join(__dirname, 'js/index.js'),
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/bundle.css'
+    })
+  ],
+  entry: path.join(__dirname, 'src/js/index.js'),
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'js/')
+    path: path.resolve(__dirname, 'dist/js/'),
   },
   module: {
     rules: [{
       test: /\.js$/,
       loader: 'babel-loader',
       exclude: '/node_modules/'
-    }
-    // {
-    //   test: /\.css$/,
-    //   use: [
-    //     { loader: mode === 'development' ?
-    //       'style-loader' : MiniCssExtractPlugin.loader },
-    //     { loader: 'css-loader' }
-    //   ]
-    // }
-    ]
+    },
+    {
+      test: /\.css$/,
+      use: [
+        { loader: mode === 'development' ?
+          'style-loader' : MiniCssExtractPlugin.loader },
+        { loader: 'css-loader' }
+      ]
+    }]
   },
   devServer: {
-    contentBase: './',
+    contentBase: './dist',
     publicPath: '/js',
     compress: true,
     stats: 'errors-only',
